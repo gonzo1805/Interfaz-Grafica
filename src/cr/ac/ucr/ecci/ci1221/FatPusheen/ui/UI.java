b8796b963d5e331528fd912672a9ed91e076cc7a
gonzo1805 <gonzo1805@gmail.com>
2016-06-10 09:39:43 -0600

    Falla al meter un panel de botones en el principal

--- src/cr/ac/ucr/ecci/ci1221/FatPusheen/ui/UI.java
+++ src/cr/ac/ucr/ecci/ci1221/FatPusheen/ui/UI.java
@@ -2,21 +2,26 @@ package cr.ac.ucr.ecci.ci1221.FatPusheen.ui;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowListener;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
 public class UI extends JFrame implements ActionListener {
 
-	private Container contenedor, contenedor2;
-	JButton btn, btn2, btn3, boton1, boton2;
+	private JFrame contenedor, contenedor2;
+	JButton queue, stack, tree, set, map, list;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -25,33 +30,62 @@ public class UI extends JFrame implements ActionListener {
 
 	public UI(int i) {
 		if (i == 1) {
-			contenedor = getContentPane();
-			contenedor.setLayout(null);
+												
+			contenedor = new JFrame();
+			setLayout(null);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
 			// Asigna un titulo a la barra de titulo
-			setTitle("Contenedor 1");
+			setTitle("Modelos");
 			// tamaño de la ventana
-			setSize(400, 300);
+			setSize(400, 350);
 			// pone la ventana en el Centro de la pantalla
 			setLocationRelativeTo(null);
-
-			btn = new JButton("Boton 1");
-			btn2 = new JButton("Escogencia");
-
-			btn2.setSize(50, 50);
-			btn.setSize(50, 50);
-			btn.setBounds(0, 50, 100, 50);
-			btn2.setBounds(0, 0, 100, 50);
-
-			btn.setEnabled(true);
-			btn2.setEnabled(true);
-
-			add(btn);
-			add(btn2);
-
-			btn2.addActionListener(new Acciones());
-			btn.addActionListener(new Acciones());
+			
+			queue = new JButton();
+			stack = new JButton();
+			tree = new JButton();
+			set = new JButton();
+			map = new JButton();
+			list = new JButton();
+			
+			queue.setText("Queue");
+			stack.setText("Stack");
+			tree.setText("Tree");
+			set.setText("Set");
+			map.setText("Map");
+			list.setText("List");
+			
+			queue.setSize(50, 50);
+			stack.setSize(50, 50);
+			tree.setSize(50, 50);
+			set.setSize(50, 50);
+			map.setSize(50, 50);
+			list.setSize(50, 50);
+			
+			queue.setBounds(0, 0, 100, 50);
+			stack.setBounds(0, 50, 100, 50);
+			tree.setBounds(0, 100, 100, 50);
+			set.setBounds(0, 150, 100, 50);
+			map.setBounds(0, 200, 100, 50);
+			list.setBounds(0, 250, 100, 50);
+					
+			contenedor.add(queue);
+			contenedor.add(stack);
+			contenedor.add(tree);
+			contenedor.add(set);
+			contenedor.add(map);
+			contenedor.add(list);
+			
+			
+			
+			
+			/*JDialog escrito = new JDialog();
+			
+			contenedor.add(escrito);
+			escrito.setBounds(200, 200, 100, 100);*/
+			
 		}
-		if (i == 2) {
+		/*if (i == 2) {
 			contenedor2 = getContentPane();
 			contenedor2.setLayout(null);
 			// Asigna un titulo a la barra de titulo
@@ -61,23 +95,8 @@ public class UI extends JFrame implements ActionListener {
 			// pone la ventana en el Centro de la pantalla
 			setLocationRelativeTo(null);
 
-			boton1 = new JButton("Nueva");
-			boton2 = new JButton("Regresar");
-
-			boton2.setSize(50, 50);
-			boton1.setSize(50, 50);
-			boton1.setBounds(0, 50, 100, 50);
-			boton2.setBounds(0, 0, 100, 50);
-
-			boton1.setEnabled(true);
-			boton2.setEnabled(true);
-
-			contenedor2.add(boton1);
-			contenedor2.add(boton2);
-
-			boton2.addActionListener(new Acciones());
-			boton1.addActionListener(new Acciones());
-		}
+			
+		}*/
 
 	}
 
@@ -90,12 +109,14 @@ public class UI extends JFrame implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			System.out.println("TROLL");
+			System.out.println("...");
 			System.out.println(e.getActionCommand());
-			if (e.getActionCommand().equals("Escogencia")) {
-				System.out.println("Eligio Escogencia");
-				UI w = new UI(2);
-				w.setVisible(true);
+			if (e.getActionCommand().equals("Queue")) {
+				JDialog escrito = new JDialog();
+				escrito.setTitle("Estructuras de queue");
+				contenedor.add(escrito);
+				
+				
 			} else if (e.getActionCommand().equals("Boton")) {
 				System.out.println("Eligio boton");
 			}
